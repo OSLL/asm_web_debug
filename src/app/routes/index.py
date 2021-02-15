c5aa4478c88f6590bd3dd48ed67f7f86d7fbc815
futurewarning <marketednihilism@gmail.com>
2021-02-15 22:29:19 +0300

    get code&breakpoints on /index

--- src/app/routes/index.py
+++ src/app/routes/index.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, make_response, render_template
+from flask import Blueprint, make_response, render_template, jsonify, request
 
 index_bp = Blueprint('index', __name__)
 bp = index_bp
 
 
 @bp.route('/')
-@bp.route('/index')
+@bp.route('/index', methods = ["POST", "GET"])
 def index():
+    if request.method == 'POST':
+        return request.form.to_dict()
     return render_template('index.html')
-
