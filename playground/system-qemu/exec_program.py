594158622a602cca3207b940f47dcb21562c6d4a
Harald <Vladimir_Aleksandrovich.01@mail.ru>
2021-02-17 14:25:05 +0300

    system-qemu-rev2

--- playground/system-qemu/exec_program.py
+++ playground/system-qemu/exec_program.py
@@ -2,27 +2,28 @@ import subprocess
 
 uc_file = "user-n001"
 
-status = subprocess.call(["as", "-32", "-g", "-o", "obj/" + uc_file, "user-src/" + uc_file + ".asm"])
+status = subprocess.call(["as", "-32", "-g", "-o", "user-space/obj/" + uc_file, "user-space/src/" + uc_file + ".asm"])
 
 if ( status != 0 ):
 	print ("compile error")
 	exit()
 
-status = subprocess.call(["ld", "-melf_i386", "-T", "linker.ld", "-o", "bin/" + uc_file, "obj/" + uc_file, "obj/loader.obj"])
+status = subprocess.call(["ld", "-melf_i386", "-T", "linker.ld", "-o", "user-space/bin/" + uc_file, "user-space/obj/" + uc_file, "user-space/loader.obj"])
 
 if ( status != 0 ):
-	print ("link error")
+	print ("linker error")
 	exit()
 
-status = subprocess.call(["ld", "-melf_i386", "-T", "linker.ld", "-o", "bin/" + uc_file, "obj/" + uc_file, "obj/loader.obj"])
+status = subprocess.call(["objcopy", "--only-keep-debug", "user-space/bin/" + uc_file, "user-space/debug-symbol/" + uc_file + ".debug"])
 
 if ( status != 0 ):
-	print ("link error")
+	print ("debug-info error")
 	exit()
-
+   
+"""
 status = subprocess.call(["qemu-system-x86_64", "-kernel", "bin/" + uc_file, "-m 1G"])
 
 if ( status != 0 ):
 	print ("start error")
 	exit()
-# -boot d -cdrom uapp.iso -m 1G
+"""
