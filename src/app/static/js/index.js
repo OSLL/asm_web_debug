732b1beed19bf13cbe871b9aba55f8afaeb76c5c
Ilya.Tsygankov <saw44947@gmail.com>
2021-02-24 14:15:57 +0300

    ver2

--- src/app/static/js/index.js
+++ src/app/static/js/index.js
@@ -2,9 +2,12 @@ function buttonClick(btn){
 	alert("You pressed " + btn.name);
 }
 
-function fonload(){
+$(function() {
 	var reg = document.querySelector('#register_body');
-	let registers = [['A1', 'B2', 'C3', 'A6'],['0xff3', '0x546', '0xaaa', '0x123']];
+	let registers = [
+		['A1', 'B2', 'C3', 'A6'],['0xff3', '0x546', '0xaaa', '0x123'], 
+		['A1', 'B2', 'C3', 'A6'],['0xff3', '0x546', '0xaaa', '0x123']
+		];
 	createTableReg(reg, registers);
 
 	var stack = document.querySelector('#stack_body');
@@ -12,18 +15,19 @@ function fonload(){
 	createTableStack(stack, s_elements);
 
 	function createTableReg(parent, arr){
-		var alen = arr[0].length;
-
-		for(var i=0; i<alen; i++){
+		for(var i=0; i<arr[0].length; i++){
 			var tr = document.createElement('tr');
-			for(var j=0; j<2; j++){
+			for(var j=0; j<4; j++){
 				var td = document.createElement('td');
-				var input = document.createElement('input');
-				input.value = arr[j][i];
-				td.appendChild(input);
+				if(j%2){
+					var input = document.createElement('input');
+					input.value = arr[j][i];
+					td.appendChild(input);
+				}
+				else td.appendChild(document.createTextNode(arr[j][i]));
 				tr.appendChild(td);
 			}
-			parent.appendChild(tr);			
+			parent.appendChild(tr);	
 		}
 	}
 
@@ -41,4 +45,4 @@ function fonload(){
 			parent.appendChild(tr);			
 		}
 	}
-}
+});
