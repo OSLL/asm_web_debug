6ccf48bf404b4d99ce771bdaed2ffda885f64fec
Dmitry Ivanov <darcenrall@gmail.com>
2021-02-25 15:59:35 +0300

    add debug + reformat

--- src/app/static/js/index.js
+++ src/app/static/js/index.js
@@ -3,36 +3,57 @@ function buttonClick(btn){
 }
 
 $(function() {
-$("#Compile").click(function (e){
-	var editor = $('.CodeMirror')[0].CodeMirror;
-	var code = editor.getValue();
-	var breakpoints = [];
-	var lines = editor.lineCount();
-	
-	for (var i = 0; i < lines; i++) {
-		var info = editor.lineInfo(i);
-		if (info.gutterMarkers)
-			breakpoints.push(i+1);
-	};
+	$("#Compile").click(function (e){
+		var editor = $('.CodeMirror')[0].CodeMirror;
+		var code = editor.getValue();
+		var breakpoints = [];
+		var lines = editor.lineCount();
+		
+		for (var i = 0; i < lines; i++) {
+			var info = editor.lineInfo(i);
+			if (info.gutterMarkers)
+				breakpoints.push(i+1);
+		};
 
-	$.ajax({
-		url: '/compile',
-		type:'POST',
-		dataType: 'json',
-		contenType: 'application/json',
-		data: {'code': code, 'breakpoints': JSON.stringify(breakpoints)},
-		success: function(){
-			console.log(code + "\n", breakpoints);
-			$("#ajax-alert").addClass('alert alert-success').text('Код успешно отправлен');
-            $("#ajax-alert").show();
-            $("#ajax-alert").delay(2000).fadeOut();
-		},
-		error: function(){
-			$("#ajax-alert").addClass('alert alert-danger').text('Код не был отправлен');
-            $("#ajax-alert").show();
-            $("#ajax-alert").delay(2000).fadeOut();
-		},
+		$.ajax({
+			url: '/compile',
+			type:'POST',
+			dataType: 'json',
+			contenType: 'application/json',
+			data: {'code': code, 'breakpoints': JSON.stringify(breakpoints)},
+			success: function(){
+				console.log(code + "\n", breakpoints);
+				$("#ajax-alert").addClass('alert alert-success').text('Код успешно отправлен');
+				$("#ajax-alert").show();
+				$("#ajax-alert").delay(2000).fadeOut();
+			},
+			error: function(){
+				$("#ajax-alert").addClass('alert alert-danger').text('Код не был отправлен');
+				$("#ajax-alert").show();
+				$("#ajax-alert").delay(2000).fadeOut();
+			},
+		});
+		e.preventDefault();
 	});
-	e.preventDefault();
-});
+
+
+	$("#Debug").click(function (e){
+		send_debug_command(e.target)		
+	});
+
+
+	function send_debug_command(button){
+		$.ajax({
+			url: '/debug',
+			type:'POST',
+			contenType: 'application/json',
+			data: {'debug_command': button.getAttribute('debug_code')},
+			success: function(response){
+				console.log(response);
+			},
+			error: function(response){
+				console.log(response);
+			},
+		});
+	}
 });
