84e7b083a20cdb37060992bf082823b242c5adce
Dmitry Ivanov <darcenrall@gmail.com>
2021-02-25 15:58:57 +0300

    add DebugCommands to config

--- src/run.py
+++ src/run.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
 from app.routes.index import index_bp
+from app.core.utils.debug_commands import DebugCommands 
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 app.register_blueprint(index_bp)
+
+app.config['DEBUG_COMMANDS'] = DebugCommands
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
