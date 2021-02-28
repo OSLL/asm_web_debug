11a24525e1dc3eb03a3d3d39530ca067746bf6e1
Harald <Vladimir_Aleksandrovich.01@mail.ru>
2021-02-28 12:11:07 +0300

    arm support_

---
+++ playground/system-qemu/start-avr.py
@@ -0,0 +1,14 @@
+
+
+
+
+"""
+
+TODO TODO TODO TODO TODO TODOTODO TODOTODO TODO TODO
+
+AVR
+
+avr-as -mmcu=atmega32u4 -g user-n002.avr.asm 
+avr-ld -m avr1 -o res a.out
+
+"""
---
+++ playground/system-qemu/start-x86.py
@@ -0,0 +1,90 @@
+"""
+
+	qemu-system-x86_64 -kernel user-space/bin/user-n001 -m 10M -s -S
+
+
+Подключение отладчика: (запуск необходимо произвести в каталоге system-qemu/)
+
+gdb
+
+target remote localhost:1234
+
+symbol-file user-space/debug-symbol/user-n001.debug    
+
+break EntryPoint 			
+
+continue		
+
+Сейчас мы находимся в исполняемом коде					
+
+////////////////
+x/10i $eip
+stepi
+nexti
+////////////////
+
+
+
+
+
+
+status = subprocess.call(["qemu-system-x86_64", "-kernel", "bin/" + uc_file, "-m 1G"])
+
+if ( status != 0 ):
+	print ("start error")
+	exit()
+"""
+
+
+
+import subprocess
+
+DIR_SRC = "user-space/src/"
+DIR_OBJ = "user-space/obj/"
+DIR_BIN = "user-space/bin/"
+DIR_DEBUG = "user-space/debug-symbol/"
+
+
+x86_AS_PARAMS = ["as", 
+				"-32", "-g"]
+
+x86_LD_PARAMS = ["ld", 
+				"-melf_i386", 
+				"-T", "linker.ld"]
+
+
+
+def compile_x86(name):
+
+	source_full_path  = DIR_SRC + name + ".S"
+	object_full_path  = DIR_OBJ + name + ".obj"
+	debug_full_path   = DIR_DEBUG + name + ".debug"
+	bin_full_path     = DIR_BIN + name + ".bin"
+
+	status = subprocess.call(x86_AS_PARAMS + [
+									"-o", object_full_path, 
+									source_full_path])
+	if ( status != 0 ):
+		print ("compile error")
+		exit(1)
+
+
+	status = subprocess.call(x86_LD_PARAMS + [
+									"-o", bin_full_path, 
+									object_full_path, 
+									"user-space/loader.obj"])
+	if ( status != 0 ):
+		subprocess.call(["rm", object_full_path])
+		print ("linker error")
+		exit(2)
+
+	status = subprocess.call(["objcopy", "--only-keep-debug", 
+									bin_full_path, 
+									debug_full_path])
+	if ( status != 0 ):
+		subprocess.call(["rm", object_full_path, bin_full_path])
+		print ("debug-info error")
+		exit(3)
+   
+
+compile_x86("user.x86")
