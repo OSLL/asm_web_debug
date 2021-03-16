4fdf87966980e22aaa7a0e6a5477bd4df48079ee
Dmitry Ivanov <darcenrall@gmail.com>
2021-03-17 01:43:10 +0300

    rename class + upd method

--- src/app/core/as_manager.py
+++ src/app/core/as_manager.py
@@ -1,6 +1,6 @@
 import subprocess
 
-class as_manager:
+class ASManager:
 
         # Constants:
 	arch_exec_path = {"x86_64" : "x86_64-linux-gnu-as",\
@@ -18,28 +18,29 @@ class as_manager:
 	# Return values:
 	#	1) [bool] if compiling was successful
 	#	2) [str] as logs
-	def compile(filename, arch):
+	@classmethod
+	def compile(cls, filename, arch):
 
 		# Setting up run flags
-		as_manager.run_flags = ["-g"]
-		if arch in as_manager.arch_run_flags:
-			as_manager.run_flags.extend(as_manager.arch_run_flags[arch])
+		cls.run_flags = ["-g"]
+		if arch in cls.arch_run_flags:
+			cls.run_flags.extend(cls.arch_run_flags[arch])
 
 		# Setting up executable path
-		if arch in as_manager.arch_exec_path:
-			as_manager.exec_path = as_manager.arch_exec_path[arch]
+		if arch in cls.arch_exec_path:
+			cls.exec_path = cls.arch_exec_path[arch]
 		else:
-			as_manager.exec_path = "as" # default assemler for system
+			cls.exec_path = "as" # default assemler for system
 
 		# Remembering information about compiled file
-		as_manager.arch = arch
-		as_manager.filename = filename
-		as_manager.output_filename = filename + ".o"
+		cls.arch = arch
+		cls.filename = filename
+		cls.output_filename = filename + ".o"
 
 		# Forming arguments to as process
-		args = [as_manager.exec_path]
-		args.extend(as_manager.run_flags)
-		args.extend([as_manager.filename, "-o", as_manager.output_filename])
+		args = [cls.exec_path]
+		args.extend(cls.run_flags)
+		args.extend([cls.filename, "-o", cls.output_filename])
 
 		# Returning response from as
 		as_resp = subprocess.run(args, capture_output = True)
