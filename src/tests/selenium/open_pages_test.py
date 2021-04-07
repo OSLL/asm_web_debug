4e4c5724dec0587b15c33fbc65ab734dda9b3eeb
Dmitry Ivanov <ivanovd.v3.0@gmail.com>
2021-04-08 00:34:12 +0300

    add hexview testing

--- src/tests/selenium/open_pages_test.py
+++ src/tests/selenium/open_pages_test.py
@@ -7,14 +7,58 @@ from basic_test import BasicTest
 
 class OpenPagesTest(BasicTest):
 
-    def test_open_slash(self):
+    def _test_open_slash(self):
         self.driver.get(self.getUrl('/'))
         code_id = self.get_code_id_from_current_url()
         assert code_id, 'No code_id in URL for new code(page)'
         assert "Web ASM" in self.driver.title, "No 'Web ASM' in title"
 
-    def test_open_code(self):
+    def _test_open_code(self):
         self.open_code_page()
         code_id = self.get_code_id_from_current_url()
         assert code_id == self.CODE_ID, 'New code_id != test.CODE_ID'
         assert "Web ASM" in self.driver.title, "No 'Web ASM' in title"
+
+    def test_open_empty_hexview(self):
+        text = ""
+        hextext = ["3C 4E 6F 20 63 6F 64 65 20 66 6F 72 20 68 65 78", "76 69 65 77 21 3E"]
+        self.set_code_and_open_hexview(text, hextext)
+
+    def test_open_hexview(self):
+        text = "Test"
+        hextext = ["54 65 73 74"]
+        self.set_code_and_open_hexview(text, hextext)
+
+    def test_open_hexview_by_get(self):
+        text = "Test"
+        hextext = ["54 65 73 74"]
+        self.set_code_and_open_hexview(text, hextext)
+
+        # hexview by get-request (load code from DB, not from post-form)
+        self.driver.get(self.getUrl(f"/hexview/{self.get_code_id_from_current_url()}"))
+
+        assert all([text in self.driver.page_source for text in hextext]), f"No '{hextext}' in hexview"
+
+    def set_code_and_open_hexview(self, text, hextext_list):
+        self.open_code_page()
+        
+        # send empty string to textarea
+        self.set_code(text)
+
+        # go to hexview
+        hexview_button = self.driver.find_element_by_id('HexView')
+        hexview_button.click()
+
+        # set active tab to hexview
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+
+        # check code_id in hex
+        code_id = self.get_code_id_from_current_url()
+        assert code_id == self.CODE_ID, 'Hexview code_id != test.CODE_ID'
+        
+        assert all([text in self.driver.page_source for text in hextext_list]), f"No '{hextext_list}' in hexview"
+
+        # set active tab to first tab
+        self.driver.close()
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        
