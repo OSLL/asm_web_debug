ec9c88760d206fe4ce30d61fb9577f0c49f5664a
Dmitry Ivanov <ivanovd.v3.0@gmail.com>
2021-04-08 00:34:59 +0300

    rename methods

--- src/tests/selenium/open_pages_test.py
+++ src/tests/selenium/open_pages_test.py
@@ -7,13 +7,13 @@ from basic_test import BasicTest
 
 class OpenPagesTest(BasicTest):
 
-    def _test_open_slash(self):
+    def test_open_slash(self):
         self.driver.get(self.getUrl('/'))
         code_id = self.get_code_id_from_current_url()
         assert code_id, 'No code_id in URL for new code(page)'
         assert "Web ASM" in self.driver.title, "No 'Web ASM' in title"
 
-    def _test_open_code(self):
+    def test_open_code(self):
         self.open_code_page()
         code_id = self.get_code_id_from_current_url()
         assert code_id == self.CODE_ID, 'New code_id != test.CODE_ID'
