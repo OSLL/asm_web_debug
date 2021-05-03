b417fbf7185c849ae412d227ef9bf8a8fec1888b
futurewarning <marketednihilism@gmail.com>
2021-05-03 23:59:47 +0300

    92 sol

--- src/app/routes/lti.py
+++ src/app/routes/lti.py
@@ -5,7 +5,12 @@ from app.core.db.manager import DBManager
 from lti.contrib.flask import FlaskToolProvider
 
 from app.core.utils.lti_validator import LTIRequestValidator
-from app.core.db.desc import Consumers
+from app.core.db.desc import Consumers, User
+
+from flask import current_app as app
+
+import flask_login
+from flask_security.datastore import UserDatastore
 
 lti_bp = Blueprint('lti', __name__ )
 bp = lti_bp
@@ -21,6 +26,14 @@ def check_request(request):
 @bp.route('/lti', methods=['POST'])
 def lti_route():
     if check_request(request):
+        temporary_user_params = request.form
+        user_id = temporary_user_params.get('user_id')
+        username = temporary_user_params.get('ext_user_username')
+        if DBManager.get_user(user_id) == None:
+            app.user_datastore.create_user(_id = user_id, username = username)
+            flask_login.login_user(User.objects.get(_id=user_id))
+        else:
+            flask_login.login_user(User.objects.get(_id=user_id))
         return redirect('/')
 
     else:
--- src/run.py
+++ src/run.py
@@ -57,17 +57,9 @@ if __name__ == "__main__":
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.objects.get(_id=user_id)
-
-    @app.before_first_request
-    def create_user():
-        user_datastore.create_user(_id='first_user', username='first_username')
-        user_datastore.create_user(_id='second_user', username='second_username')
-
-        if app.config['ANON_ACCESS']:
-            user_datastore.create_user(_id=app.config['ANON_USER_ID'], username='anon_username')
-
-        user = user_datastore.find_user(_id='first_user')
-        flask_login.login_user(user)
+        try: 
+            return User.objects.get(_id=user_id)
+        except User.DoesNotExist:
+            return None
 
     run_app(app)
