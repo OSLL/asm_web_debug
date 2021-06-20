6ccba27f646e7812a61bd972d9cb206434686b86
Dmitry Ivanov <ivanovd.v3.0@gmail.com>
2021-06-20 13:57:03 +0300

    class for response (as dict w/timestamp)

---
+++ src/app/response.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+
+class Response(dict):
+  
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs,
+                        timestamp=datetime.utcnow().strftime("%H:%M:%S %d.%m.%Y"))
