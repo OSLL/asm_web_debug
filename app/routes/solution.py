d4ef94745787d3aef83738b74c6b07f506453127
Ciratoku <edissonman255@gmail.com>
2022-03-19 20:12:11 +0300

    get solution function, SQLAlchemy was removed

--- app/routes/solution.py
+++ app/routes/solution.py
@@ -1,14 +1,19 @@
 from flask import Blueprint, render_template, redirect, request
-from flask_sqlalchemy import SQLAlchemy
  
+from app.core.db.manager import get_solution
+
 class Solution:
-    id = 1
-    task = "Task"
+    def __init__(self, solution):
+        self.id = solution._id
+        self.datetime = solution.datetime
+        self.feedback = solution.feedback
+        self.task = solution.task
+        self.LTI_session = solution.LTI_session
 
 
 def getSolutionBy(id):
     #get solution by id from db
-    return Solution()
+    return Solution(get_solution(id))
 
 
 solution_bp = Blueprint('solution', __name__)
