26cb19f77911295dab8c36c71d660a99eb4dff8b
Artem Revin <artezka@gmail.com>
2022-04-19 19:26:38 +0300

    Fix solution bp (weird conflict resolved)

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ from app.routes.logs import log_bp
 from app.routes.lti import lti_bp
 from app.routes.welcome import welcome_bp
 from app.routes.tasks import tasks_bp
+from app.routes.solution import solution_bp
 from app.routes.admin_view import adminview_bp
 from app.config import ConfigManager
 
@@ -29,7 +30,7 @@ def create_app():
     app.register_blueprint(debug_bp)
     app.register_blueprint(welcome_bp)
     app.register_blueprint(tasks_bp)
-    app.register_blueprint(solutions_bp)
+    app.register_blueprint(solution_bp)
     app.register_blueprint(adminview_bp)
 
     # load config
